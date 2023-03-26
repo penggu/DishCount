a065eec89d592d6403baf9b143d4e3e103fd34db
Peng Gu <penggu@gmail.com>
2023-03-26 10:38:34 -0700

    camera enabled, update main.dart and pubspec.yaml

--- dishcount/lib/main.dart
+++ dishcount/lib/main.dart
@@ -1,52 +1,163 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+// import 'package:path_provider_platform_interface/path_provider_platform_interface.dart';
+// import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
-void main() {
-  runApp(DishCount());
+Future<void> main() async {
+  // Ensure that plugin services are initialized so that `availableCameras()`
+  // can be called before `runApp()`
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Obtain a list of the available cameras on the device.
+  final cameras = await availableCameras();
+
+  // Get a specific camera from the list of available cameras.
+  final firstCamera = cameras.first;
+
+  runApp(
+    MaterialApp(
+      theme: ThemeData.dark(),
+      home: TakePictureScreen(
+        // Pass the appropriate camera to the TakePictureScreen widget.
+        camera: firstCamera,
+      ),
+    ),
+  );
 }
 
-class DishCount extends StatefulWidget {
-  const DishCount({Key? key}) : super(key: key);
+class TakePictureScreen extends StatefulWidget {
+  final CameraDescription camera;
+
+  const TakePictureScreen({
+    Key? key,
+    required this.camera,
+  }) : super(key: key);
 
   @override
-  State<DishCount> createState() => _DishCountState();
+  TakePictureScreenState createState() => TakePictureScreenState();
 }
 
-class _DishCountState extends State<DishCount> {
-  int count = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text('Dish Count'),
-          ),
-          body: Center(
-            child: Text('Hello 🌏! ' + '$count'), 
-          ),
-          floatingActionButton: FloatingActionButton(
-            child: Icon(Icons.add),
-            onPressed: () {
-              setState(() {
-                count++;
-              });
-            },
-          )),
-    );
-  }
+class TakePictureScreenState extends State<TakePictureScreen> {
+  late CameraController _controller;
+  late Future<void> _initializeControllerFuture;
 
   @override
   void initState() {
-    // TODO: fetch data from database
-    
     super.initState();
-    // print('initState');
+    // To display the current output from the Camera,
+    // create a CameraController.
+    _controller = CameraController(
+      // Get a specific camera from the list of available cameras.
+      widget.camera,
+      // Define the resolution to use.
+      ResolutionPreset.veryHigh,
+    );
+
+    // Next, initialize the controller. This returns a Future.
+    _initializeControllerFuture = _controller.initialize();
   }
 
   @override
   void dispose() {
-    // TODO: implement dispose
-
+    // Dispose of the controller when the widget is disposed.
+    _controller.dispose();
     super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Take a picture')),
+      body: FutureBuilder<void>(
+        future: _initializeControllerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            // If the Future is complete, display the preview.
+            return CameraPreview(_controller);
+          } else {
+            // Otherwise, display a loading indicator.
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.camera_alt),
+        // Provide an onPressed callback.
+        onPressed: () async {
+          try {
+            // Ensure that the camera is initialized.
+            await _initializeControllerFuture;
+
+            // Construct the path where the image should be saved.
+            final path = join(
+              // Find the temp directory using the `path_provider` plugin.
+              (await getTemporaryDirectory()).path,
+              '${DateTime.now()}.png',
+            );
+
+            // Attempt to take a picture and log where it's been saved.
+            await _controller.takePicture();
+
+            // If the picture was taken, navigate to the PreviewScreen.
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => PreviewScreen(imagePath: path),
+              ),
+            );
+          } catch (e) {
+            // If an error occurs, log the error to the console.
+            print(e);
+          }
+        },
+      ),
+    );
+  }
+}
+
+class PreviewScreen extends StatelessWidget {
+  final String imagePath;
+
+  const PreviewScreen({Key? key, required this.imagePath}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Preview'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.file(File(imagePath)),
+            SizedBox(height: 32),
+            ElevatedButton(
+              child: Text('Save Image'),
+              onPressed: () => _saveImage(context),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> _saveImage(BuildContext context) async {
+    final directory = await getApplicationDocumentsDirectory();
+    final name = basename(imagePath);
+    final newPath = '${directory.path}/$name';
+    final newFile = File(newPath);
+
+    await File(imagePath).copy(newPath);
+
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text('Image saved to $newPath'),
+      duration: Duration(seconds: 2),
+    ));
   }
 }
